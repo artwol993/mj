8e704c4a435d75fb60fcaf4baa876ab9a8c7470a
Artur <artwol993@gmail.com>
2024-01-27 14:55:00 +0100

    up

---
+++ src/components/about.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="container about-container">
+      <h3>Trochę o mnie</h3>
+    </div>
+  );
+}
+
+export default About;
