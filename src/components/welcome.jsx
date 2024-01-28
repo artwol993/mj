728ab0c20da5a7373c7a487116637b1e8c7cfae4
Artur <artwol993@gmail.com>
2024-01-28 20:15:12 +0100

    update fonts

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -48,24 +48,26 @@ function Welcome() {
   }, [showBottomSection]);
 
   return (
-    <div
-      className={`container welcome-container ${
-        showBottomSection ? "show-bottom" : ""
-      }`}
-      id="welcome"
-    >
-      <div className="top-section">
-        <div
-          className="logo-container"
-          style={{ backgroundImage: `url(${logoBackground})` }}
-        ></div>
-      </div>
-      <div className="bottom-section">
-        <div className="name">
-          <p>{title}</p> <h1>Marzanna Jakoniuk</h1>
+    <>
+      <div
+        className={`container welcome-container ${
+          showBottomSection ? "show-bottom" : ""
+        }`}
+        id="welcome"
+      >
+        <div className="top-section">
+          <div
+            className="logo-container"
+            style={{ backgroundImage: `url(${logoBackground})` }}
+          ></div>
+        </div>
+        <div className="bottom-section">
+          <div className="name">
+            <p>{title}</p> <h1>Marzanna Jakoniuk</h1>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
