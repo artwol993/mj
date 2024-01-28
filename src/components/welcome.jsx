e2a5baf23fecc0015b8bfd5fe602098980190571
Artur <artwol993@gmail.com>
2024-01-28 12:24:33 +0100

    add interval to tittle and add responsible layout

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -6,6 +6,9 @@ import mj from "../assets/mj.jpg";
 function Welcome() {
   const [showBottomSection, setShowBottomSection] = useState(false);
   const [logoBackground, setLogoBackground] = useState(logo);
+  const [title, setTitle] = useState("Psychoterapeuta");
+
+  const titles = ["Psychoterapeuta", "Psycholog", "Terapeuta TSR"];
 
   useEffect(() => {
     const adjustWelcomeHeight = () => {
@@ -30,6 +33,20 @@ function Welcome() {
     return () => clearTimeout(timeout);
   }, []);
 
+  useEffect(() => {
+    if (showBottomSection) {
+      const interval = setInterval(() => {
+        setTitle((prevTitle) => {
+          const currentIndex = titles.indexOf(prevTitle);
+          const nextIndex = (currentIndex + 1) % titles.length;
+          return titles[nextIndex];
+        });
+      }, 2000); // Zmiana tytułu co 2 sekundy
+
+      return () => clearInterval(interval);
+    }
+  }, [showBottomSection]);
+
   return (
     <div
       className={`container welcome-container ${
@@ -45,8 +62,7 @@ function Welcome() {
       </div>
       <div className="bottom-section">
         <div className="name">
-          <p>Psychoterapeuta</p>
-          <h1>Marzanna Jakoniuk</h1>
+          <p>{title}</p> <h1>Marzanna Jakoniuk</h1>
         </div>
       </div>
     </div>
