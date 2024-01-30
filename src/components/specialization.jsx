fff23f69030a6df43f81fbaaf7c53e500deb92ad
Artur <artwol993@gmail.com>
2024-01-30 21:05:53 +0100

    add section

---
+++ src/components/specialization.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Specialization() {
+  return (
+    <>
+      <div id="specialization">Metody leczenia</div>
+    </>
+  );
+}
+
+export default Specialization;
