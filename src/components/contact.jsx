139b6d1418aed7d68f944a00b6db87c53686eec8
Artur <artwol993@gmail.com>
2024-02-01 22:16:54 +0100

    add mobile version

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -13,41 +13,34 @@ function Contact() {
               <div className="map">
                 <div style={{ width: "100%" }}>
                   <iframe
-                    width="100%"
-                    height="200"
-                    frameBorder="0"
-                    scrolling="yes"
-                    marginHeight="0"
-                    marginWidth="0"
+
                     src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=ul.Wiertnicza%20118,%20Warszawa+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
                   ></iframe>
                 </div>
+                <p>ul.Wiertnicza 118, Warszawa</p>
               </div>
-              <p>ul.Wiertnicza 118, Warszawa</p>
+
               <div className="map">
                 <div style={{ width: "100%" }}>
                   <iframe
-                    width="100%"
-                    height="200"
-                    frameborder="0"
-                    scrolling="yes"
-                    marginheight="0"
-                    marginwidth="0"
                     src="https://maps.google.com/maps?width=100%25&amp;height=200&amp;hl=en&amp;q=Emilii%20Plater%2047,%20Warszawa+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
                   ></iframe>
                 </div>
+                <p>ul.Emilii Plater 47, Warszawa</p>
               </div>
-              <p>ul.Emilii Plater 47, Warszawa</p>
             </div>
           </div>
 
           <div className="contact-box">
             <div className="contact-box-row">
               <a href="mailto:marzanna.jakoniuk@psychoterapia.pl">
-                <h5>email: marzanna.jakoniuk@psychoterapia.pl</h5>
+                <h5 className="contact-link">
+                  {" "}
+                  marzanna.jakoniuk@psychoterapia.pl
+                </h5>
               </a>
               <a href="tel:+48509019090">
-                <h5>telefon: +48 509 019 090</h5>
+                <h5 className="contact-link">+48 509 019 090</h5>
               </a>
             </div>
           </div>
@@ -55,20 +48,24 @@ function Contact() {
           <div className="contact-box">
             <h4>Zapraszam do zapisu online</h4>
             <div className="contact-box-row">
-              <a
-                href="https://booksy.com/pl-pl/133194_poradnia-co-tam-wilanow_psychoterapia_3_warszawa/staffer/272186#ba_s=bd_1"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src="src/assets/Booksy-logo.svg" alt="Booksy" />
-              </a>
-              <a
-                href="https://www.znanylekarz.pl/marzanna-jakoniuk/psycholog/warszawa#"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src="../assets/znanylekarz-logo.jpg" alt="Znany Lekarz" />
-              </a>
+              <div className="contact-box">
+                <a
+                  href="https://booksy.com/pl-pl/133194_poradnia-co-tam-wilanow_psychoterapia_3_warszawa/staffer/272186#ba_s=bd_1"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="contact-icon booksy"></div>
+                </a>
+              </div>
+              <div className="contact-box">
+                <a
+                  href="https://www.znanylekarz.pl/marzanna-jakoniuk/psycholog/warszawa#"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="contact-icon zl"></div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
