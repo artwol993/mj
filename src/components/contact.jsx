4f3ac52672f52d403901924ac8920cd396ce368e
Artur <artwol993@gmail.com>
2024-02-01 22:36:01 +0100

    add tablet version

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -7,24 +7,19 @@ function Contact() {
       <div className="contact-section" id="contact">
         <div className="contact-container">
           <h3>Kontakt</h3>
+
           <div className="contact-box">
             <h4>Gdzie przyjmuję?</h4>
             <div className="contact-box-row">
               <div className="map">
                 <div style={{ width: "100%" }}>
-                  <iframe
-
-                    src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=ul.Wiertnicza%20118,%20Warszawa+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-                  ></iframe>
+                  <iframe src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=ul.Wiertnicza%20118,%20Warszawa+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
                 </div>
                 <p>ul.Wiertnicza 118, Warszawa</p>
               </div>
-
               <div className="map">
                 <div style={{ width: "100%" }}>
-                  <iframe
-                    src="https://maps.google.com/maps?width=100%25&amp;height=200&amp;hl=en&amp;q=Emilii%20Plater%2047,%20Warszawa+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-                  ></iframe>
+                  <iframe src="https://maps.google.com/maps?width=100%25&amp;height=200&amp;hl=en&amp;q=Emilii%20Plater%2047,%20Warszawa+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
                 </div>
                 <p>ul.Emilii Plater 47, Warszawa</p>
               </div>
@@ -33,9 +28,9 @@ function Contact() {
 
           <div className="contact-box">
             <div className="contact-box-row">
+              
               <a href="mailto:marzanna.jakoniuk@psychoterapia.pl">
                 <h5 className="contact-link">
-                  {" "}
                   marzanna.jakoniuk@psychoterapia.pl
                 </h5>
               </a>
@@ -48,18 +43,18 @@ function Contact() {
           <div className="contact-box">
             <h4>Zapraszam do zapisu online</h4>
             <div className="contact-box-row">
-              <div className="contact-box">
+              <div className="contact-box-inner">
                 <a
-                  href="https://booksy.com/pl-pl/133194_poradnia-co-tam-wilanow_psychoterapia_3_warszawa/staffer/272186#ba_s=bd_1"
+                  href="https://booksy.com/pl-pl/..."
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <div className="contact-icon booksy"></div>
                 </a>
               </div>
-              <div className="contact-box">
+              <div className="contact-box-inner">
                 <a
-                  href="https://www.znanylekarz.pl/marzanna-jakoniuk/psycholog/warszawa#"
+                  href="https://www.znanylekarz.pl/..."
                   target="_blank"
                   rel="noopener noreferrer"
                 >
