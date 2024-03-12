5e9dce60a8dd4fff5d0b1a56881a89690bd7d021
Artur <artwol993@gmail.com>
2024-03-12 20:26:24 +0100

    update tablet version, restore header and paragraph

--- src/components/offer.jsx
+++ src/components/offer.jsx
@@ -41,6 +41,19 @@ function Offer() {
 
   return (
     <section className="offer-section">
+      <h3 className="offer-header">
+        Czego spodziewać się podczas spotkań terapii indywidualnej?
+      </h3>
+      <p className="offer-paragraph">
+        Każde spotkanie terapii indywidualnej trwa 50 minut i odbywa się raz w
+        tygodniu, w ustalony dzień i godzinę. Pierwsze 2-3 spotkania to
+        spotkania konsultacyjne. Na pierwszym spotkaniu standardowo zadaję
+        szereg pytań dotyczących sytuacji życiowej zgłaszającej się osoby oraz
+        doświadczanych przez nią trudności, z którymi chciałaby poradzić sobie w
+        terapii. Podczas kolejnych spotkań jest czas dla obu stron na podjęcie
+        decyzji co do dalszej współpracy. Jest to także przestrzeń na wstępne
+        sformułowanie celów terapeutycznych zgłaszającej się osoby.
+      </p>
       <div className="offer-container">
         {Object.keys(modalData).map((title) => (
           <div
