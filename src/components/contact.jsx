dc1463085dacb3ad04a33ffde459706453896c7c
Artur <artwol993@gmail.com>
2024-03-14 00:53:31 +0100

    contact done

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -8,18 +8,19 @@ function Contact() {
         <h3 className="contact-header">Kontakt</h3>
         <div className="contact-container">
           <div className="contact-box">
-            <div className="contact-box-row">
-              <div className="map">
-                <p className="map-paragraph">
-                  Przyjmuję stacjonarnie w Warszawie Miedzeszyn,{" "}
-                  <b> ul. Akacjowa 4</b>
-                </p>
-                <div>
+            <div className="contact-box-background">
+              <div className="contact-box-row">
+                <div className="map-container">
+                  <p className="map-paragraph">
+                    Przyjmuję stacjonarnie w Warszawie Miedzeszyn, ul. Akacjowa
+                    4
+                  </p>
+                </div>
+              </div>
+              <div className="contact-box-row">
+                <div className="map">
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5649.889256183399!2d20.998597155499343!3d52.20536030460206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecccf837dfe5f%3A0x10a3621ae11bcc8f!2sAkacjowa%204%2C%2002-534%20Warszawa!5e0!3m2!1spl!2spl!4v1710278182016!5m2!1spl!2spl"
-                    // width="600"
-                    // height="450"
-                    // style={{ border: 0 }}
                     allowFullScreen=""
                     loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"
@@ -29,30 +30,34 @@ function Contact() {
             </div>
           </div>
           <div className="contact-box__2">
-            <div className="contact-box-row__2">
-              <div>
+            <div className="contact-box-background">
+              <div className="contact-box-row__2">
                 <a className="contact-box-link" href="tel:+48509019090">
                   <div className="icon phone-icon"></div>
                   <div className="contact-link-p">
                     <p>+48 509 019 090</p>
                   </div>
                 </a>
-                <a
-                  className="contact-box-link"
-                  href="mailto:marzanna.jakoniuk@psychoterapia.pl"
-                >
-                  <div className="icon email-icon"></div>
-                  <p className="contact-link-p">
-                    marzanna.jakoniuk@psychoterapia.pl
-                  </p>
-                </a>
               </div>
-            </div>
-            <div className="contact-box-row__2">
-              <p className="paragraph-online">
-                Pracuję także <b>online</b>, przez komunikator, skype lub google
-                meet
-              </p>
+              <div className="contact-box-row__2">
+                <div>
+                  <a
+                    className="contact-box-link"
+                    href="mailto:marzanna.jakoniuk@psychoterapia.pl"
+                  >
+                    <div className="icon email-icon"></div>
+                    <p className="contact-link-p">
+                      marzanna.jakoniuk@psychoterapia.pl
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <div className="contact-box-row__2">
+                <p className="paragraph-online">
+                  Pracuję także <b>online</b>, przez komunikator, skype lub
+                  google meet
+                </p>
+              </div>
               <div className="contact-box-row__icons">
                 <div className="icon skype-icon"></div>
                 <div className="icon gm-icon"></div>
