c1663531c328f8dc611b00712619ba16fb3d2172
Artur <artwol993@gmail.com>
2024-03-13 23:04:40 +0100

    add mobile version

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -14,12 +14,12 @@ function Contact() {
                   Przyjmuję stacjonarnie w Warszawie Miedzeszyn,{" "}
                   <b> ul. Akacjowa 4</b>
                 </p>
-                <div style={{ width: "100%" }}>
+                <div>
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5649.889256183399!2d20.998597155499343!3d52.20536030460206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecccf837dfe5f%3A0x10a3621ae11bcc8f!2sAkacjowa%204%2C%2002-534%20Warszawa!5e0!3m2!1spl!2spl!4v1710278182016!5m2!1spl!2spl"
-                    width="600"
-                    height="450"
-                    style={{ border: 0 }}
+                    // width="600"
+                    // height="450"
+                    // style={{ border: 0 }}
                     allowFullScreen=""
                     loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"
@@ -29,7 +29,7 @@ function Contact() {
             </div>
           </div>
           <div className="contact-box__2">
-            <div className="contact-box-row">
+            <div className="contact-box-row__2">
               <div>
                 <a className="contact-box-link" href="tel:+48509019090">
                   <div className="icon phone-icon"></div>
@@ -48,7 +48,7 @@ function Contact() {
                 </a>
               </div>
             </div>
-            <div className="contact-box-row">
+            <div className="contact-box-row__2">
               <p className="paragraph-online">
                 Pracuję także <b>online</b>, przez komunikator, skype lub google
                 meet
