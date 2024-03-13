53cb20578b64cab9729d42d5323414dfe09ced40
Artur <artwol993@gmail.com>
2024-03-13 18:13:47 +0100

    mobile version

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -5,23 +5,28 @@ function Contact() {
   return (
     <>
       <div className="contact-section" id="contact">
+        <h3 className="contact-header">Kontakt</h3>
         <div className="contact-container">
-          <h3>Kontakt</h3>
+   
 
           <div className="contact-box">
-            <h4>Gdzie przyjmuję?</h4>
             <div className="contact-box-row">
               <div className="map">
                 <div style={{ width: "100%" }}>
-                  <iframe src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=ul.Wiertnicza%20118,%20Warszawa+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5649.889256183399!2d20.998597155499343!3d52.20536030460206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecccf837dfe5f%3A0x10a3621ae11bcc8f!2sAkacjowa%204%2C%2002-534%20Warszawa!5e0!3m2!1spl!2spl!4v1710278182016!5m2!1spl!2spl"
+                    width="600"
+                    height="450"
+                    style={{ border: 0 }}
+                    allowFullScreen=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                  ></iframe>
                 </div>
-                <p>ul.Wiertnicza 118, Warszawa</p>
-              </div>
-              <div className="map">
-                <div style={{ width: "100%" }}>
-                  <iframe src="https://maps.google.com/maps?width=100%25&amp;height=200&amp;hl=en&amp;q=Emilii%20Plater%2047,%20Warszawa+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-                </div>
-                <p>ul.Emilii Plater 47, Warszawa</p>
+                <p className="map-paragraph">
+                  Przyjmuję stacjonarnie w Warszawie Miedzeszyn,{" "}
+                  <b> ul. Akacjowa 4</b>
+                </p>
               </div>
             </div>
           </div>
@@ -29,37 +34,18 @@ function Contact() {
           <div className="contact-box">
             <div className="contact-box-row">
               <a href="mailto:marzanna.jakoniuk@psychoterapia.pl">
-                <h5 className="contact-link">
+                <p className="contact-link">
                   marzanna.jakoniuk@psychoterapia.pl
-                </h5>
+                </p>
               </a>
               <a href="tel:+48509019090">
-                <h5 className="contact-link">+48 509 019 090</h5>
+                <p className="contact-link">+48 509 019 090</p>
               </a>
             </div>
-          </div>
-
-          <div className="contact-box">
-            <h4>Zapraszam do zapisu online</h4>
             <div className="contact-box-row">
-              <div className="contact-box-inner">
-                <a
-                  href="https://booksy.com/pl-pl/133194_poradnia-co-tam-wilanow_psychoterapia_3_warszawa/staffer/272186#ba_s=bd_1"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <div className="contact-icon booksy"></div>
-                </a>
-              </div>
-              <div className="contact-box-inner">
-                <a
-                  href="https://www.znanylekarz.pl/marzanna-jakoniuk/psycholog/warszawa"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <div className="contact-icon zl"></div>
-                </a>
-              </div>
+              <p className="paragraph-online">
+                Pracuję także online, przez komunikator, skype lub google meet
+              </p>
             </div>
           </div>
         </div>
