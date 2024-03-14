1cce399049fd3f7b300f3bd2978813564c9b0a5e
Artur <artwol993@gmail.com>
2024-03-14 21:45:39 +0100

    add style to text

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -21,22 +21,35 @@ function About() {
         >
           <div className="about-text-box">
             <p style={{ textIndent: "20px" }}>
-              Jestem psychoterapeutką w trakcie szkolenia w Szkole Psychoterapii
-              Gestalt, która posiada akredytację EAGT (European Association of
-              Gestalt Therapy), jest członkiem European Association for
-              Psychotherapy (EAP) i spełnia wymogi „Ustawy o Niektórych Zawodach
-              Medycznych”. Jestem także psycholożką, terapeutką TSR i trenerką
-              grupową.
-              <br></br>
-              <br></br>
+              Jestem psychoterapeutką w trakcie szkolenia
+              <b>w Szkole Psychoterapii Gestalt,</b> która posiada akredytację
+              <b> EAGT</b> (European Association of Gestalt Therapy), jest
+              członkiem European Association for Psychotherapy <b>(EAP)</b> i
+              spełnia wymogi „Ustawy o Niektórych Zawodach Medycznych”. Jestem
+              także <b>psycholożką, terapeutką TSR i trenerką grupową.</b>
+            </p>
+          </div>
+          <div className="about-text-box">
+            <p>
               Doświadczenie zawodowe kształtowałam współpracując z Fundacją
               Itaka, w Centrum Wsparcia dla Osób w Kryzysie Psychicznym, w
               Centrum Wsparcia i Mediacji dla Rodzin, Samodzielnym Wojewódzkim
               Zespole Publicznych Zakładów Psychiatrycznej Opieki Zdrowotnej w
               Warszawie, oraz przez 20 lat w pracy z młodzieżą i młodymi
               dorosłymi w szkole średniej.
-              <br></br>
-              <br></br>
+            </p>
+          </div>
+        </motion.div>
+        <motion.div
+          className="about-box"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 1 }}
+          variants={boxVariants}
+        >
+          <div className="about-text-box">
+            <p>
               Ponieważ wierzę, że człowiek kształci się przez całe życie, nadal
               doskonalę swoje umiejętności podczas szkoleń i warsztatów
               prowadzonych przez certyfikowanych trenerów różnych podejść
@@ -58,16 +71,22 @@ function About() {
               osobami dorosłymi. Superwizuję swoją praktykę indywidualną u
               certyfikowanych superwizorów. Kieruję się Kodeksami Etyki
               Psychologa i Polskiego Towarzystwa Psychoterapii Gestalt.
-              <br></br>
-              <br></br>W pracy psychoterapeutki ważna jest dla mnie otwartość
-              oraz uważność na drugiego człowieka, które pomagają budować
-              autentyczną relację, opartą na szacunku i współodczuwaniu.
-              Towarzyszę ludziom w podróży do satysfakcjonującego życia,
-              nazywania potrzeb i rozpoznawania składników potencjału,
-              akceptowania siebie. Aby odzyskiwali kontrolę, doświadczali
-              sprawczości, przezwyciężali kryzys.
-              <br></br>
-              <br></br>
+            </p>
+          </div>
+
+          <div className="about-text-box">
+            <p>
+              W pracy psychoterapeutki ważna jest dla mnie otwartość oraz
+              uważność na drugiego człowieka, które pomagają budować autentyczną
+              relację, opartą na szacunku i współodczuwaniu. Towarzyszę ludziom
+              w podróży do satysfakcjonującego życia, nazywania potrzeb i
+              rozpoznawania składników potencjału, akceptowania siebie. Aby
+              odzyskiwali kontrolę, doświadczali sprawczości, przezwyciężali
+              kryzys.
+            </p>
+          </div>
+          <div className="about-text-box">
+            <p>
               Wierzę, że akceptujący kontakt jest początkiem zmiany a każdy
               człowiek ma wystarczające zasoby i umiejętności żeby poradzić
               sobie z trudnościami.
@@ -82,15 +101,32 @@ function About() {
           transition={{ duration: 1 }}
           variants={boxVariants}
         >
+          <div className="about-box-picture"></div>
+        </motion.div>
+        <motion.div
+          className="about-box"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 1 }}
+          variants={boxVariants}
+        >
           <div className="about-text-box">
             <p style={{ textIndent: "20px" }}>
               Ponieważ gotowe rozwiązania nie istnieją, zapraszam do wspólnych
               poszukiwań.
-              <br></br>
-              <br></br>
+            </p>
+          </div>
+
+          <div className="about-text-box">
+            <p>
               Bogate doświadczenia zawodowe i osobiste pomagają mi lepiej
               zrozumieć świat moich klientów.
-              <br></br> <br></br>
+            </p>
+          </div>
+
+          <div className="about-text-box">
+            <p>
               Prywatnie jestem żoną i mamą. W wolnym czasie lubię kontakt z
               naturą, piesze wędrówki i wycieczki rowerowe.
             </p>
