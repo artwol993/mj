f8888318d077bebcd2293e38cacf4e9037d31bb8
Artur <artwol993@gmail.com>
2024-03-19 00:20:46 +0100

    add tablet design

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -12,7 +12,7 @@ function About() {
       <h2>O mnie</h2>
       <div className="about-container">
         <motion.div
-          className="about-box exp"
+          className="about-box"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true }}
@@ -60,9 +60,10 @@ function About() {
           transition={{ duration: 1 }}
           variants={boxVariants}
         >
+          <div className="about-box-image"></div>
           <div className="about-text-box">
-            <p>
-              Ponieważ wierzę, że{" "}
+            <p style={{ textIndent: "20px" }}>
+              Ponieważ wierzę, że
               <b> człowiek kształci się przez całe życie, </b>
               nadal doskonalę swoje umiejętności podczas szkoleń i warsztatów
               prowadzonych przez certyfikowanych trenerów różnych podejść
@@ -78,18 +79,8 @@ function About() {
           transition={{ duration: 1 }}
           variants={boxVariants}
         >
-          <div className="about-text-box white-box">
-            <p style={{ textIndent: "20px" }}>
-              Pracuję indywidualnie z <b>młodzieżą</b> w wieku od 16 roku życia
-              oraz z <b>osobami dorosłymi</b>. Superwizuję swoją praktykę
-              indywidualną u certyfikowanych superwizorów. Kieruję się Kodeksami{" "}
-              <b>Etyki Psychologa </b> i
-              <b> Polskiego Towarzystwa Psychoterapii Gestalt</b>.
-            </p>
-          </div>
-
           <div className="about-text-box">
-            <p>
+            <p style={{ textIndent: "20px" }}>
               W pracy psychoterapeutki ważna jest dla mnie <b> otwartość </b>
               oraz <b> uważność na drugiego człowieka, </b> które pomagają
               budować autentyczną relację, opartą na szacunku i współodczuwaniu.
@@ -101,6 +92,25 @@ function About() {
             </p>
           </div>
           <div className="about-text-box white-box">
+            <p>
+              Pracuję indywidualnie z <b>młodzieżą</b> w wieku od 16 roku życia
+              oraz z <b>osobami dorosłymi</b>. Superwizuję swoją praktykę
+              indywidualną u certyfikowanych superwizorów. Kieruję się Kodeksami{" "}
+              <b>Etyki Psychologa </b> i
+              <b> Polskiego Towarzystwa Psychoterapii Gestalt</b>.
+            </p>
+          </div>
+        </motion.div>
+        <motion.div
+          className="about-box"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 1 }}
+          variants={boxVariants}
+        >
+          <div className="about-box-image"></div>
+          <div className="about-text-box">
             <p>
               Wierzę, że akceptujący kontakt jest początkiem zmiany a każdy
               człowiek ma wystarczające zasoby i umiejętności żeby poradzić
@@ -115,7 +125,29 @@ function About() {
           viewport={{ once: true }}
           transition={{ duration: 1 }}
           variants={boxVariants}
-        ></motion.div>
+        >
+          <div className="about-text-box ">
+            <p className="white-text">
+              Ponieważ gotowe rozwiązania nie istnieją, zapraszam do wspólnych
+              poszukiwań.
+            </p>
+          </div>
+        </motion.div>
+        <motion.div
+          className="about-box"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 1 }}
+          variants={boxVariants}
+        >
+          <div className="about-text-box">
+            <p className="white-text">
+              Bogate doświadczenia zawodowe i osobiste pomagają mi lepiej
+              zrozumieć świat moich klientów.
+            </p>
+          </div>
+        </motion.div>
         <motion.div
           className="about-box"
           initial="hidden"
@@ -125,22 +157,8 @@ function About() {
           variants={boxVariants}
         >
           <div className="about-text-box ">
-            <p className="white-text">
-              Ponieważ gotowe rozwiązania nie istnieją,
-              <b> zapraszam do wspólnych poszukiwań. </b>
-            </p>
-          </div>
-
-          <div className="about-text-box">
-            <p className="white-text">
-              Bogate doświadczenia zawodowe i osobiste pomagają mi lepiej
-              zrozumieć świat moich klientów.
-            </p>
-          </div>
-
-          <div className="about-text-box white-box">
             <div className="about-box-picture"></div>
-            <p>
+            <p className="white-text">
               Prywatnie jestem żoną i mamą. W wolnym czasie lubię kontakt z
               naturą, piesze wędrówki i wycieczki rowerowe.
             </p>
