3b9d1d37c4a5d06d17e43f92d7d7468796a4ce5f
Artur <artwol993@gmail.com>
2024-03-24 18:53:09 +0100

    update about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -21,142 +21,54 @@ function About() {
         >
           <div className="about-text-box">
             <p>
-              Jestem psychoterapeutką w trakcie szkolenia
-              <b> w Szkole Psychoterapii Gestalt,</b> która posiada akredytację
-              <b> EAGT </b> (European Association of Gestalt Therapy), jest
-              członkiem European Association for Psychotherapy <b> (EAP) </b> i
-              spełnia wymogi „Ustawy o Niektórych Zawodach Medycznych”. Jestem
-              także <b> psycholożką, terapeutką TSR i trenerką grupową. </b>
+              Jestem <b> psychologiem </b> i <b> psychoterapeutką </b> w trakcie
+              4-letniego szkolenia w Szkole Psychoterapii Gestalt, która posiada
+              akredytację EAGT (European Association of Gestalt Therapy) i jest
+              członkiem European Association for Psychotherapy (EAP). Ukończyłam
+              także kurs podstawowy i zaawansowany Terapii Skoncentrowanej na
+              Rozwiązaniach (TSR).
             </p>
-          </div>
-          <div className="about-text-box white-box">
             <p>
-              <b>Doświadczenie zawodowe kształtowałam współpracując z:</b>
+              <b>Doświadczenie zawodowe </b> kształtowałam współpracując z
+              Fundacją Itaka, w Centrum Wsparcia dla Osób Dorosłych w Kryzysie
+              Psychicznym, w Pogotowiu Psychologicznym, w Centrum Wsparcia i
+              Mediacji dla Rodzin a także podczas stażu w Samodzielnym
+              Wojewódzkim Zespole Publicznych Zakładów Psychiatrycznej Opieki
+              Zdrowotnej w Warszawie.
             </p>
-            <ul className="exp-ul">
-              <li className="exp-li">Fundacją Itaka, </li>
-              <li className="exp-li">
-                w Centrum Wsparcia dla Osób w Kryzysie Psychicznym,
-              </li>
-              <li className="exp-li">
-                w Centrum Wsparcia i Mediacji dla Rodzin,
-              </li>
-              <li className="exp-li">
-                Samodzielnym Wojewódzkim Zespole Publicznych Zakładów
-                Psychiatrycznej Opieki Zdrowotnej w Warszawie,
-              </li>
-              <li className="exp-li">
-                młodzieżą i młodymi dorosłymi w szkole średniej przez ponad 20
-                lat.
-              </li>
-            </ul>
-          </div>
-        </motion.div>
-        <motion.div
-          className="about-box"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          transition={{ duration: 1 }}
-          variants={boxVariants}
-        >
-          <div className="about-box-image pic-therapy"></div>
-          <div className="about-text-box">
             <p>
-              Ponieważ wierzę, że człowiek kształci się przez całe życie,
-              <b>
-                {" "}
-                nadal doskonalę swoje umiejętności podczas szkoleń i warsztatów{" "}
-              </b>
+              Ponieważ wierzę, że człowiek kształci się przez całe życie, nadal
+              doskonalę swoje umiejętności podczas szkoleń i warsztatów
               prowadzonych przez certyfikowanych trenerów różnych podejść
               psychoterapeutycznych.
             </p>
-          </div>
-        </motion.div>
-        <motion.div
-          className="about-box"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          transition={{ duration: 1 }}
-          variants={boxVariants}
-        >
-          <div className="about-text-box">
-            <p style={{ textIndent: "20px" }}>
+            <p>
+              Superwizuję swoją praktykę indywidualną u certyfikowanych
+              superwizorów. Kieruję się Kodeksami Etyki Zawodowej Psychologa i
+              Polskiego Towarzystwa Psychoterapii Gestalt.
+            </p>
+            <p>
               W pracy psychoterapeutki ważna jest dla mnie <b> otwartość </b>
-              oraz <b> uważność na drugiego człowieka, </b> które pomagają
-              budować autentyczną relację, opartą na szacunku i współodczuwaniu.
-              Towarzyszę ludziom w podróży do satysfakcjonującego życia,
-              <b> nazywania potrzeb </b>i rozpoznawania składników potencjału,{" "}
-              <b> akceptowania siebie. </b>
-              Aby odzyskiwali kontrolę, doświadczali sprawczości, przezwyciężali
-              kryzys.
+              oraz <b> uważność </b> na drugiego człowieka, które pomagają
+              budować <b> autentyczną relację, </b> opartą na <b> szacunku </b>{" "}
+              i <b> współodczuwaniu </b>. Towarzyszę ludziom w podróży do
+              satysfakcjonującego życia, nazywania potrzeb i rozpoznawania
+              składników potencjału, akceptowania siebie. Aby odzyskiwali
+              kontrolę, doświadczali sprawczości, przezwyciężali kryzys, uczyli
+              się samoregulacji. Wierzę, że akceptujący kontakt jest początkiem
+              zmiany a każdy człowiek ma wystarczające zasoby i umiejętności
+              żeby poradzić sobie z trudnościami.
             </p>
-          </div>
-          <div className="about-text-box white-box">
             <p>
-              Pracuję indywidualnie z <b>młodzieżą</b> w wieku od 16 roku życia
-              oraz z <b>osobami dorosłymi</b>. Superwizuję swoją praktykę
-              indywidualną u certyfikowanych superwizorów. Kieruję się
-              <b> Kodeksami Etyki Psychologa </b> i
-              <b> Polskiego Towarzystwa Psychoterapii Gestalt</b>.
-            </p>
-          </div>
-        </motion.div>
-        <motion.div
-          className="about-box"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          transition={{ duration: 1 }}
-          variants={boxVariants}
-        >
-          <div className="about-box-image pic-change"></div>
-          <div className="about-text-box">
-            <p>
-              Wierzę, że akceptujący kontakt jest początkiem zmiany a każdy
-              człowiek ma wystarczające zasoby i umiejętności żeby poradzić
-              sobie z trudnościami.
-            </p>
-          </div>
-        </motion.div>
-        <motion.div
-          className="about-box"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          transition={{ duration: 1 }}
-          variants={boxVariants}
-        >
-          <div className="about-text-box ">
-            <p className="white-text">
-              Ponieważ gotowe rozwiązania nie istnieją, zapraszam do wspólnych
-              poszukiwań.
-            </p>
-            <p className="white-text">
-              Bogate doświadczenia zawodowe i osobiste pomagają mi lepiej
-              zrozumieć świat moich klientów.
-            </p>
-          </div>
-        </motion.div>
-
-        <motion.div
-          className="about-box"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          transition={{ duration: 1 }}
-          variants={boxVariants}
-        >
-          <div className="about-text-box ">
-            <div className="about-box-picture"></div>
-            <p className="white-text">
-              Prywatnie jestem żoną i mamą. W wolnym czasie lubię kontakt z
-              naturą, piesze wędrówki i wycieczki rowerowe.
+              Pracuję indywidualnie z <b> osobami dorosłymi </b> i z{" "}
+              <b> młodzieżą </b> w wieku od 16 roku życia. <b> Każda osoba</b>,
+              bez względu na płeć, wyznanie, kolor skóry, orientację jest u mnie
+              <b> mile widziana </b>.
             </p>
           </div>
         </motion.div>
       </div>
+      
     </div>
   );
 }
