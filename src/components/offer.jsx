6d7c8907c494d3af7eb89bb72fa83d9365f1541b
Artur <artwol993@gmail.com>
2024-04-02 22:30:42 +0200

    add type section

--- src/components/offer.jsx
+++ src/components/offer.jsx
@@ -1,13 +1,9 @@
-import React, { useState } from "react";
-import Modal from "./modal";
+import React from "react";
 import { motion } from "framer-motion";
 import Price from "./price";
+import Type from "./type";
 
 function Offer() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalTitle, setModalTitle] = useState("");
-  const [modalContent, setModalContent] = useState("");
-
   const containerVariants = {
     hidden: { opacity: 0, y: 50 },
     visible: {
@@ -17,48 +13,13 @@ function Offer() {
     },
   };
 
-  const modalData = {
-    "Konsultacja psychologiczna":
-      "To rozmowa, podczas której psycholog zbiera informacje dotyczące zgłaszanych problemów, omawia z osobą zgłaszającą się na konsultację bieżące dolegliwości emocjonalne, relacyjne, rodzinne i ich historię. Nie proponuje gotowych rozwiązań ale pomaga dobrać najbardziej odpowiedni rodzaj dalszej pracy (np. terapia grupowa, terapia indywidualna, terapia pary, mediacje, leczenie psychiatryczne, itp.)",
-    "Terapia krótkoterminowa":
-      "Trwa zazwyczaj kilka miesięcy i koncentruje się na rozwiązaniu jednego zgłaszanego problemu. Jest użyteczna gdy potrzebna jest pomoc w jednej konkretnej sytuacji życiowej. Praca terapeutyczna koncentruje się na odkrywaniu i rozwijaniu zasobów oraz umiejętności osoby szukającej wsparcia. Terapeuta towarzyszy w poszukiwaniu nowych sposobów radzenia sobie z problemem i wprowadzaniu w życie zmian.",
-    "Terapia długoterminowa":
-      "To regularne spotkania, których cel określany jest podczas kilku pierwszych sesji. Nastawiona jest na poznanie siebie, własnych potrzeb, zrozumienie ograniczających mechanizmów, które uniemożliwiają pełne korzystanie z własnego potencjału. Dzięki relacji terapeutycznej, terapeutycznemu przymierzu osoba w psychoterapii uczy się nazywać własne potrzeby, odpowiedzialnie i z szacunkiem do własnych możliwości (uwzględniając emocje, myśli i ciało), realizować je w świcie. Moje doświadczenie pokazuje, że psychoterapia długoterminowa trwa powyżej jednego roku.  ",
-    "Terapia młodzieży (16+)":
-      "Osoby w wieku 16-18 lat na pierwsze spotkanie zapraszam z osobą dorosłą, rodzicem lub opiekunem. Pierwsze spotkanie to rozmowa psychoterapeutki z nastolatkiem i opiekunem. Jej celem jest poznanie oczekiwań zgłaszających się osób i motywacji nastolatka do terapii. Spotkanie z opiekunem jest także ważne ze względów formalnych – to rodzic wyraża zgodę na terapię osoby poniżej osiemnastego roku życia. Kolejne spotkania odbywają się już z nastolatkiem indywidualnie.",
-  };
-
-  const openModal = (title) => {
-    setModalTitle(title);
-    setModalContent(modalData[title]);
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
     <section className="offer-section" id="offer">
       <h3 className="offer-price-header">Oferta</h3>
       //price-section
       <Price />
       //type-section
-      <div className="offer-type-container">
-        {Object.keys(modalData).map((title) => (
-          <div
-            className="offer-type-box"
-            onClick={() => openModal(title)}
-            key={title}
-          >
-            <h4 className="offer-type-box__title">{title}</h4>
-            <div className="offer-type-box__picture"></div>
-          </div>
-        ))}
-      </div>
-      <Modal show={isModalOpen} onHide={closeModal} title={modalTitle}>
-        <p>{modalContent}</p>
-      </Modal>
+      <Type />
       //info section
       <motion.div
         className="offer-info-container"
