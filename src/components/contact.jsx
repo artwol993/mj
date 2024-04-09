b1ff94bb270c9e58db8e6f095a5f5f7b5ecc9dd5
Artur <artwol993@gmail.com>
2024-04-09 19:07:33 +0200

    add contact style

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -7,24 +7,6 @@ function Contact() {
       <div className="contact-section" id="contact">
         <h3 className="contact-header">Kontakt</h3>
         <div className="contact-container">
-          <p className="map-paragraph">
-            Przyjmuję stacjonarnie w Warszawie Miedzeszyn, ul. Agrestowa 4
-          </p>
-          <div className="map-container">
-            <div className="map">
-              <iframe
-                width="100%"
-                height="600"
-                frameborder="0"
-                scrolling="no"
-                marginheight="0"
-                marginwidth="0"
-                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Agrestowa%204%20warszawa+(Psychoterapia%20-%20Marzanna%20Jakoniuk%20)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-              >
-                <a href="https://www.gps.ie/">gps devices</a>
-              </iframe>
-            </div>
-          </div>
           <div className="contact-box">
             <div className="contact-box-row">
               <a className="contact-box-link" href="tel:+48509019090">
@@ -47,11 +29,30 @@ function Contact() {
             </div>
             <div className="contact-box-row">
               <p className="paragraph-online">
-                Pracuję także <b>online</b>, przez komunikator, skype lub google
-                meet
+                Pracuję także <b>online</b>, przez komunikator
               </p>
             </div>
           </div>
+          <p className="map-paragraph">
+            Przyjmuję stacjonarnie w <br></br>Warszawie Miedzeszyn, ul.
+            Agrestowa 4
+          </p>
+          <div className="map-container">
+            <div className="map">
+              <iframe
+                width="100%"
+                height="600"
+                frameborder="0"
+                scrolling="no"
+                marginheight="0"
+                marginwidth="0"
+                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Agrestowa%204%20warszawa+(Psychoterapia%20-%20Marzanna%20Jakoniuk%20)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+              >
+                <a href="https://www.gps.ie/">gps devices</a>
+              </iframe>
+            </div>
+          </div>
+
           <div className="contact-color"></div>
         </div>
       </div>
