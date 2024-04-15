62cf7413c584cae8a6950a8e261637e85e9e6f3c
Artur <artwol993@gmail.com>
2024-04-15 22:35:02 +0200

    update sierotki w services

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -18,14 +18,14 @@ function Services() {
             <div className="services-box-text">
               <p className="services-box-p">
                 Psychoterapia Gestalt jest jednym z wiodących podejść nurtu
-                humanistycznego. Oznacza to, że w centrum jej zainteresowania
-                stoi człowiek, jego zasoby oraz relacja międzyludzka.
-                Współczesna psychoterapia Gestalt zakłada, że{" "}
+                humanistycznego. Oznacza to, <br></br> że w centrum jej
+                zainteresowania stoi człowiek, jego zasoby oraz relacja
+                międzyludzka. Współczesna psychoterapia Gestalt zakłada, że{" "}
                 <b> każdy człowiek ma </b> w sobie <b> potencjał do rozwoju </b>{" "}
                 i ma możliwość satysfakcjonującego życia w wybranym środowisku.
                 Jest to możliwe do osiągnięcia przy spełnieniu warunku
                 świadomego funkcjonowania, czyli zauważania i rozumienia tego,
-                co się dzieje w nas, z nami i dookoła nas.
+                co się dzieje w nas, <br></br> z nami i dookoła nas.
               </p>
             </div>
           </div>
@@ -38,8 +38,9 @@ function Services() {
                 Klienta/Klientkę świadomości siebie, po to, żeby w dalszej
                 perspektywie móc samodzielnie się wyregulować: uświadamiać sobie
                 własne emocje, podejmować autonomiczne decyzje w oparciu o
-                potrzeby i pragnienia. Słowem, tak realizować siebie w świecie,
-                tak się z nim kontaktować, aby nie cierpieć.
+                potrzeby <br></br> i pragnienia. Słowem, tak realizować siebie{" "}
+                <br></br> w świecie, tak się z nim kontaktować, <br></br> aby
+                nie cierpieć.
               </p>
             </div>
             <div className="services-box-image image-2"></div>
